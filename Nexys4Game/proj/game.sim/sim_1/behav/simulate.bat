1d03b9f4355c5033b350251795fe883d646e826a
Mitchell Gu <me@mitchgu.com>
2015-11-17 01:17:45 -0500

    fix gitignore for vivado project files

--- Nexys4Game/proj/game.sim/sim_1/behav/simulate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=C:\\Xilinx\\Vivado\\2015.3\\bin
-call %xv_path%/xsim SC_note_matching_sub_tb_behav -key {Behavioral:sim_1:Functional:SC_note_matching_sub_tb} -tclbatch SC_note_matching_sub_tb.tcl -log simulate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
