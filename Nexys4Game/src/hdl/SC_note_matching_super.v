d1c63614090c0f111575b9beecf6e3f6619376f5
Ryan Berg <ryanberg@mit.edu>
2015-11-17 00:31:27 -0500

    completed SC_super draft

--- Nexys4Game/src/hdl/SC_note_matching_super.v
+++ Nexys4Game/src/hdl/SC_note_matching_super.v
@@ -1,14 +1,31 @@
 module SC_note_matching_super(
     input clk,
     input [36:0] NDATA,
+    input [37*18-1:0] metadata_link,
     
-    
-    output reg [36:0] match_time[17:0]
+    output reg [36:0] metadata_request,
+    output reg [36:0] match_trigger,
+    output reg [37*18-1:0] match_time
     );
     
     reg [36:0] note_prev; //array of previous note-states
     wire [36:0] note_edge; //note edge transitions detected
     
+    assign note_edge = (NDATA&~note_prev); //detects 0 -> transition on NDATA
     
+    //create 37 note sub_modules
+    genvar i;
+    generate
+        for (i = 0; i < 36; i = i + 1) begin : gen_loop
+            SC_note_matching_sub note_matcher(.clk(clk), .song_time(song_time),
+                                                .note_edge(note_edge[i]), .note_time(metadata_link[18*i+17:18*i]),
+                                                .note_request(metadata_request[i]), .match_enable(match_trigger[i]),
+                                                .match_time(match_trigger[18*i+17:18*i]));
+        end
+    endgenerate
     
+    always @(posedge clk) begin
+        note_prev <= NDATA; //update previous note-state
+    end
+        
 endmodule
