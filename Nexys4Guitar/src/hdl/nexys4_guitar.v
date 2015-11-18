19c4eec5beed0c317588d4fe7a70f7ba0aca69bc
Mitchell Gu <me@mitchgu.com>
2015-11-18 01:01:55 -0500

    Oversample 16x and 256x and use switch[0[ to toggle the output

--- Nexys4Guitar/src/hdl/nexys4_guitar.v
+++ Nexys4Guitar/src/hdl/nexys4_guitar.v
@@ -45,24 +45,55 @@ module nexys4_guitar (
     clk_100_to_104mhz clockgen(
         .clk_in1(CLK100MHZ),
         .clk_out1(clk_104mhz));
-    
-    // INSTANTIATE XADC OVERSAMPLING MODULE
-    wire [15:0] oversample;
-    wire oversample_done;
-    xadc_oversample256 oversampler(
-        .clk(clk_104mhz),
-        .vp(guitar_p),
-        .vn(guitar_n),
-        .en(1),
-        .reset(0),
-        .oversample(oversample),
-        .done(oversample_done));
 
+
+    wire [15:0] sample_reg;
+    wire eoc, reset_xadc;
+    // INSTANTIATE XADC GUITAR 1 INPUT
+    xadc_guitar xadc_guitar_1 (
+        .dclk_in(clk_104mhz),         // Master clock for DRP and XADC. 
+        .di_in(0),             // DRP input info (0 becuase we don't need to write)
+        .daddr_in(6'h13),      // The DRP register address for the third analog aux
+        .den_in(1),            // DRP enable line high (we want to read)
+        .dwe_in(0),            // DRP write enable low (never write)
+        .drdy_out(),           // DRP ready signal (unused)
+        .do_out(sample_reg),   // DRP output from register (the ADC data)
+        .reset_in(reset_xadc),      // reset line
+        .vp_in(0),             // dedicated/built in analog channel on bank 0
+        .vn_in(0),             // can't use this analog channel b/c of nexys 4 setup
+        .vauxp3(guitar_p),           // The third analog auxiliary input channel
+        .vauxn3(guitar_n),           // Choose this one b/c it's on JXADC header 1
+        .channel_out(),        // Not useful in sngle channel mode
+        .eoc_out(eoc),         // Pulses high on end of ADC conversion
+        .alarm_out(),          // Not useful
+        .eos_out(),            // End of sequence pulse, not useful
+        .busy_out()            // High when conversion is in progress. unused.
+    );
+
+    wire [13:0] osample16;
+    wire done_osample16;
+    oversample16 osamp16_1 (
+        .clk(clk_104mhz),
+        .sample(sample_reg[15:4]),
+        .eoc(eoc),
+        .oversample(osample16),
+        .done(done));
+
+    wire [15:0] osample256;
+    wire done_osample256;
+    oversample256 osamp256_1 (
+        .clk(clk_104mhz),
+        .sample(sample_reg[15:4]),
+        .eoc(eoc),
+        .oversample(osample256),
+        .done(done));
+
+    wire [10:0] pwm_sample;
     // INSTANTIATE PWM AUDIO OUT MODULE
     // This is a PWM frequency of around 51kHz.
     pwm11 guitar_pwm(
         .clk(clk_104mhz),
-        .PWM_in(oversample[15:5]),
+        .PWM_in(pwm_sample),
         .PWM_out(guitar_audio_pwm),
         .PWM_sd(guitar_audio_sd)
         );
@@ -123,9 +154,14 @@ module nexys4_guitar (
     assign guitar_p = AD3P;
     assign guitar_n = AD3N;
 
+    // PWM input is 16x or 256x oversampled depending on switch 0
+    assign pwm_sample = SW[0] ? osample16[13:3] : osample256[15:5];
     // Connect the guitar pwm audio to Nexys's PWM out
     assign AUD_PWM = guitar_audio_pwm;
     assign AUD_SD = guitar_audio_sd;
+
+    // Use center button to reset xadc
+    assign reset_xadc = center_button;
 
     // Debounce all buttons
     assign reset_debounce = 0;
--- Nexys4Guitar/src/hdl/oversampler.v
+++ Nexys4Guitar/src/hdl/oversampler.v
@@ -20,60 +20,64 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-
-module xadc_oversample256(
+module oversample16(
     input clk,
-    input vp,
-    input vn,
-    input en,
-    input reset,
-    output reg [15:0] oversample,
+    input [11:0] sample,
+    input eoc,
+    output reg [13:0] oversample,
     output reg done
     );
 
-    wire eoc;
-    wire [15:0] sample_reg;
-    reg [7:0] counter = 0;
-    reg [19:0] accumulator = 0;
-
-    // INSTANTIATE XADC GUITAR 1 INPUT
-    xadc_guitar xadc_guitar_1 (
-        .dclk_in(clk),         // Master clock for DRP and XADC. 
-        .di_in(0),             // DRP input info (0 becuase we don't need to write)
-        .daddr_in(6'h13),      // The DRP register address for the third analog aux
-        .den_in(1),            // DRP enable line high (we want to read)
-        .dwe_in(0),            // DRP write enable low (never write)
-        .drdy_out(),           // DRP ready signal (unused)
-        .do_out(sample_reg),   // DRP output from register (the ADC data)
-        .reset_in(reset),      // reset line
-        .vp_in(0),             // dedicated/built in analog channel on bank 0
-        .vn_in(0),             // can't use this analog channel b/c of nexys 4 setup
-        .vauxp3(vp),           // The third analog auxiliary input channel
-        .vauxn3(vn),           // Choose this one b/c it's on JXADC header 1
-        .channel_out(),        // Not useful in sngle channel mode
-        .eoc_out(eoc),         // Pulses high on end of ADC conversion
-        .alarm_out(),          // Not useful
-        .eos_out(),            // End of sequence pulse, not useful
-        .busy_out()            // High when conversion is in progress. unused.
-    );
+    reg [3:0] counter = 0;
+    reg [15:0] accumulator = 0;
 
     always @(posedge clk) begin
         if (eoc) begin
             // Conversion has ended and we can read a new sample
-            if (&counter) begin // If counter is full (256 accumulated)
-                // Get final total, divide by 16 with rounding.
-                oversample <= (accumulator + counter + 4'b0111) >> 4;
+            if (&counter) begin // If counter is full (16 accumulated)
+                // Get final total, divide by 4 with (very limited) rounding.
+                oversample <= (accumulator + sample + 2'b10) >> 2;
                 done <= 1;
                 // Reset accumulator
                 accumulator <= 0;
             end
             else begin
                 // Else add to accumulator as usual
-                accumulator <= accumulator + sample_reg;
+                accumulator <= accumulator + sample;
                 done <= 0;
             end
             counter <= counter + 1;
         end
     end
+endmodule
 
+module oversample256(
+    input clk,
+    input [11:0] sample,
+    input eoc,
+    output reg [15:0] oversample,
+    output reg done
+    );
+
+    reg [7:0] counter = 0;
+    reg [19:0] accumulator = 0;
+
+    always @(posedge clk) begin
+        if (eoc) begin
+            // Conversion has ended and we can read a new sample
+            if (&counter) begin // If counter is full (256 accumulated)
+                // Get final total, divide by 16 with rounding.
+                oversample <= (accumulator + sample + 4'b0111) >> 4;
+                done <= 1;
+                // Reset accumulator
+                accumulator <= 0;
+            end
+            else begin
+                // Else add to accumulator as usual
+                accumulator <= accumulator + sample;
+                done <= 0;
+            end
+            counter <= counter + 1;
+        end
+    end
 endmodule
