f6f7c47e72682347c9b9c3b81681d7c8ddf34614
Ryan Berg <ryanberg@mit.edu>
2015-11-28 02:45:59 -0500

    fleshed out CL block

--- Nexys4Game/proj/README.md
+++
@@ -1,1 +0,0 @@
-The Vivado project folder should go here.
--- Nexys4Game/src/hdl/CL_block.v
+++ Nexys4Game/src/hdl/CL_block.v
@@ -1,9 +1,26 @@
 module CL_block(
+    input clk, //100mhz clk
+    input pause_SW, //Debounced switch input for pause
+    input reset_button, //Debounced button for reset
     
+    output reset, //status signal for reset
+    output pause, //status signal for game-pause
+    output song_time //current song_time
+    );
     
+    CL_fsm fsm (
+        .clk(clk),
+        .pause_SW(pause_SW),
+        .reset_trigger(reset_button),
+        .pause(pause),
+        .reset(reset)
+    );
     
-    
-    
+    CL_song_timer timer (
+        .clk(clk),
+        .pause(pause),
+        .reset(reset),
+        .song_time(song_time)
     );
     
     
