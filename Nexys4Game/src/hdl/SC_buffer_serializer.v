fa4ca2a9b3b7bd9544172a2b6f67c24cc53db236
Ryan Berg <ryanberg@mit.edu>
2015-11-28 12:14:28 -0500

    progress commit for buffer_serializer. Stills has some kinks to work out.

--- Nexys4Game/src/hdl/SC_buffer_serializer.v
+++ Nexys4Game/src/hdl/SC_buffer_serializer.v
@@ -12,12 +12,14 @@ module SC_buffer_serializer(
     reg [17*6-1:0] match_queue; //represents a shift register with 6 shifts of 17 bits each, where the MSB means a valid match needs to be scored, and the next 16 bits are the dt
     
     initial match_queue = 0;
-    
+                
     always @(posedge clk) begin
         if(match_trigger != 0) begin //there's at least 1 match
+            //FIXME
+            integer i = 0;
             for(i = 0; i < 37; i = i + 1) begin
                 if(match_trigger[i] == 1'b1) begin //match on ith note
-                    match_queue[17*6-1:17*5] = {1'b1,match_time[i*16+15:i*16]}; //WILL FAIL IF TWO MATCHES IN SAME CLOCK CYCLE (will only take the highest match)
+                    match_queue[17*6-1:17*5] <= {1'b1,match_time[i*16+15:i*16]}; //WILL FAIL IF TWO MATCHES IN SAME CLOCK CYCLE (will only take the highest match)
                 end
             end
         end
